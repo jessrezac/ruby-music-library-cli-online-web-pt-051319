a5ab8e7642af8e75175943512f8070bc2840ca6e
Jess Rezac <jess.rezac@gmail.com>
2019-06-12 22:51:58 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -66,6 +66,7 @@ class MusicLibraryController
     songs = Song.all.select do |song|
       song.artist.name == user_input
     end.sort {|left,right| left.name <=> right.name}
+    binding.pry
 
     songs.each_with_index do |song, i|
       puts "#{i+1}. #{song.name} - #{song.genre.name}"
