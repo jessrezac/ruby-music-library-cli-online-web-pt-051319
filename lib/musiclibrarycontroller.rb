287c08f17f6ca1e1fa40bd7d368c36c8e8ee4b82
Jess Rezac <jess.rezac@gmail.com>
2019-06-12 23:02:38 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -65,7 +65,7 @@ class MusicLibraryController
     input = gets.chomp
 
     songs = Song.all.select {|song| song.artist.name == input }.sort {|left,right| left.name <=> right.name}
-    
+
 binding.pry
     songs.each_with_index do |song, i|
       puts "#{i+1}. #{song.name} - #{song.genre.name}"
