f51a928f02db62592bd4d248526a3ae5522d48c7
Jess Rezac <jess.rezac@gmail.com>
2019-06-12 22:58:51 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -63,7 +63,7 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.chomp
-    
+
     songs = Song.all.select {|song| song.artist.name == user_input }
 
     binding.pry
