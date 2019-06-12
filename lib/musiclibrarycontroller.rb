a35f1b32f2a1d28a8756d2ab9bfc5702a8d75220
Jess Rezac <jess.rezac@gmail.com>
2019-06-12 23:26:37 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -88,7 +88,7 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     input = gets.chomp
 
-    if input <= Song.all.length
+    if Song.all.length > = 
       song = list_songs.find.with_index {|song, i| i + 1 == input}
     end
 
