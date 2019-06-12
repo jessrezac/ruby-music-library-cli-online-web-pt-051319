2a525c9e9180ed56c1a655c168277b725080d329
Jess Rezac <jess.rezac@gmail.com>
2019-06-12 22:57:48 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -63,11 +63,8 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     user_input = gets.chomp
-    songs = Song.all.select do |song|
-      binding.pry
-
-      song.artist.name == user_input
-    end
+    
+    songs = Song.all.select {|song| song.artist.name == user_input }
 
     songs.sort {|left,right| left.name <=> right.name}
 
